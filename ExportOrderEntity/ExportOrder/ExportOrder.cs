ed6ea7e4e4bd5a79cda6623fe21148f15d211fbe
Sergey Danilov <sdanilov@axionplus.ru>
2023-09-12 15:27:48 +0300

    12-09 PM

--- ExportOrderEntity/ExportOrder/ExportOrder.cs
+++ ExportOrderEntity/ExportOrder/ExportOrder.cs
@@ -1,5 +1,6 @@
 ﻿using ExportOrderEntites.Cntr;
 using ExportOrderEntites.Document;
+using ExportOrderEntites.VesselCall;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ExportOrderEntites.ExportOrder;
@@ -10,7 +11,7 @@ public class ExportOrderEntity : Entity
 #pragma warning disable CS8618 // Поле, не допускающее значения NULL, должно содержать значение, отличное от NULL, при выходе из конструктора. Возможно, стоит объявить поле как допускающее значения NULL.
     public string Num { get; set; }
     public DateTime Dated { get; set; } = DateTime.Today;
-
+    public VesselCallEntity VesselCall { get; set; }
     public CarrierCatalog  Carrier { get; set; }
 
     public List<DocumentEntity>? Documents { get; set; } = new List<DocumentEntity>(); 
